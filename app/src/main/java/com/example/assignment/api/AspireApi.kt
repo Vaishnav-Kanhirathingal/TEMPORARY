99aa724cd350168e6d507897c045d88aff55c7cf
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-09-30 10:38:37 +0530

    retrofit support and api added

---
+++ app/src/main/java/com/example/assignment/api/AspireApi.kt
@@ -0,0 +1,26 @@
+package com.example.assignment.api
+
+import com.example.assignment.data.ListScreen
+import com.example.assignment.data.UserData
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.PUT
+
+private const val baseURL = "https://aspireinfotechs.in/filmdial_app/public/api/auth/"
+
+private val retrofit: Retrofit = Retrofit
+    .Builder()
+    .baseUrl(baseURL)
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+val service: AspireApi by lazy { retrofit.create(AspireApi::class.java) }
+
+interface AspireApi {
+    @PUT("register")
+    fun register(userData: UserData)
+
+    @GET("get_department")
+    fun getDepartment(): ListScreen
+}
