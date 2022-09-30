7474d0765e752571c7694c0fcbd85e20abd56bb5
Vaishnav Kanhirathingal <vaishnav.kanhira@gmail.com>
2022-09-30 11:15:55 +0530

    list screen added, network call successful

---
+++ app/src/main/java/com/example/assignment/DisplayActivity.kt
@@ -0,0 +1,40 @@
+package com.example.assignment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import com.example.assignment.api.service
+import com.example.assignment.data.Data
+import com.example.assignment.databinding.ActivityDisplayBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class DisplayActivity : AppCompatActivity() {
+    private val TAG = this::class.java.simpleName
+    private lateinit var binding: ActivityDisplayBinding
+    private val listScreen: MutableLiveData<List<Data>> = MutableLiveData(mutableListOf())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDisplayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        applyBinding()
+        getData()
+    }
+
+    private fun applyBinding() {
+        listScreen.observe(this) {
+            // TODO: submit list
+        }
+    }
+
+    private fun getData() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val temp = service.getDepartment()
+            listScreen.value = temp.data
+            Log.d(TAG, "temp.status: ${temp.status} | temp.message: ${temp.message}")
+        }
+    }
+}
