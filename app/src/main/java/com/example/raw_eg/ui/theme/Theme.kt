8ad158425748d6165acf5d0b0e0df397af58cb3a
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-02-04 22:28:16 +0530

    changes for adding new project

--- app/src/main/java/com/example/raw_eg/ui/theme/Theme.kt
+++ app/src/main/java/com/example/raw_eg/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.raw_eg.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
