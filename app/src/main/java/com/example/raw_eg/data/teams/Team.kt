b121fa31123369a954f3cf29f056ce804b075a18
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-02-05 03:05:40 +0530

    major UI changes

--- app/src/main/java/com/example/raw_eg/data/teams/Team.kt
+++ app/src/main/java/com/example/raw_eg/data/teams/Team.kt
@@ -5,8 +5,8 @@ import com.google.gson.annotations.SerializedName
 class Team(
     @SerializedName("uid") val uid: String,
     @SerializedName("year") val year: String,
-    @SerializedName("tid") val tid: String,
+    @SerializedName("tid") val teamId: String,
     @SerializedName("tn") val tn: String,
     @SerializedName("ta") val ta: String,
-    @SerializedName("logo") val logo: String,
+    @SerializedName("logo") val logoURL: String,
 )
--- app/src/main/java/com/example/raw_eg/ui/home/SchedulePage.kt
+++ app/src/main/java/com/example/raw_eg/ui/home/SchedulePage.kt
@@ -6,12 +6,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,7 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
+import coil3.compose.AsyncImage
 import com.example.raw_eg.CustomSharedValues
 import com.example.raw_eg.MainViewModel
 import com.example.raw_eg.data.schedule.Schedule
@@ -119,6 +121,8 @@ object SchedulePage {
 
                 Column(
                     modifier = Modifier.padding(all = 16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                     content = {
                         Text(
                             modifier = Modifier.fillMaxWidth(),
@@ -127,25 +131,65 @@ object SchedulePage {
                         )
                         Row(
                             modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically,
                             content = {
                                 Column(
                                     modifier = Modifier.weight(1f),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center,
                                     content = {
-
+                                        AsyncImage(
+                                            modifier = Modifier.size(size = 60.dp),
+                                            model = viewModel.teamList.find { it.teamId == schedule.visitorTeam.teamId }?.logoURL,
+                                            contentDescription = null,
+                                        )
+                                        if (isFinal) {
+                                            Text(text = schedule.visitorTeam.teamAlias)
+                                        }
                                     }
                                 )
 
+                                Text(
+                                    modifier = Modifier.weight(1f),
+                                    textAlign = TextAlign.Center,
+                                    text = schedule.visitorTeam.let { if (isFinal) it.score else it.teamAlias })
+                                Text(
+                                    modifier = Modifier.padding(horizontal = 24.dp),
+                                    text = if (isFinal) "@" else "VS"
+                                )
+                                Text(
+                                    modifier = Modifier.weight(1f),
+                                    textAlign = TextAlign.Center,
+                                    text = schedule.homeTeam.let { if (isFinal) it.score else it.teamAlias })
 
-                                Text(text = if (isFinal) "@" else "VS")
+                                Column(
+                                    modifier = Modifier.weight(1f),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center,
+                                    content = {
+                                        AsyncImage(
+                                            modifier = Modifier.size(size = 60.dp),
+                                            model = viewModel.teamList.find { it.teamId == schedule.homeTeam.teamId }?.logoURL,
+                                            contentDescription = null,
+                                        )
+                                        if (isFinal) {
+                                            Text(text = schedule.homeTeam.teamAlias)
+                                        }
+                                    }
+                                )
+
                             }
                         )
-
-
-
-
-
-                        Text(text = schedule.gameTime)
-                        Text("game = ${schedule.visitorTeam.score} to ${schedule.homeTeam.score}")
+                        if (!isFinal) {
+                            Button(
+                                modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                                onClick = { TODO() },
+                                content = {
+                                    Text(text = "BUY TICKETS ON TICKETMASTER")
+                                }
+                            )
+                        }
                     }
                 )
             }
