b8a6233cee5fd5c72001bf69e2c57b04c5c5876d
Vaishnav-Kanhirathingal <vaishnav.kanhira@gmail.com>
2025-04-16 16:22:01 +0530

    service created

---
+++ App_3/app/src/main/java/com/example/composetest/network/OneApiService.kt
@@ -0,0 +1,31 @@
+package com.example.composetest.network
+
+import com.google.gson.JsonObject
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface OneApiService {
+    /**
+     * https://www.freetestapi.com/api/v1/weathers
+     * https://www.freetestapi.com/api/v1/weathers/{id}
+     * */
+    companion object {
+        private const val BASE_URL = "https://www.freetestapi.com/api/v1/"
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(OneApiService::class.java)
+
+    }
+
+    @GET("weathers/")
+    suspend fun getWeatherList(): List<JsonObject>
+
+    @GET("weathers/{id}/")
+    suspend fun getWeather(@Path("id") id: Int): JsonObject
+
+}
